4e09b3e4afe82777f2e205945b960c53d5019c35
wagonsoftware <vlad.s@wagon.llc>
2022-11-09 14:53:13 +0300

    Initial commit

---
+++ apps/grossular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'grossular-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'grossular';
+}
